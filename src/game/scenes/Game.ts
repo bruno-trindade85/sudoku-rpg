696351aa04d54754f66f00b33a54d9ea9ebb13de
bruno-trindade85 <brunocmt@gmail.com>
2026-09-25 14:04:08 -0300

    feat: preload dragon idle spritesheet

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -81,7 +81,7 @@ export class Game extends Scene {
         CHARACTER_TYPES.forEach((character) => {
             this.load.image(UNIT_TEXTURES[character.id], `assets/units/${UNIT_TEXTURES[character.id]}.png`);
         });
-        this.load.image('dragon', 'assets/enemies/dragon.png');
+        this.load.spritesheet('dragon-idle-right', 'assets/enemies/dragon/idle-right.png', {\n            frameWidth: 88,\n            frameHeight: 99\n        });
     }
 
     create() {
